4b934b9a385f0b9fdfaef46219d0fefad32b025c
DontDDoS <git@dontddos.me>
2026-07-18 07:45:27 +0100

    Created netbox regions

---
+++ netbox-regions.tf
@@ -0,0 +1,13 @@
+resource "netbox_region" "europe" {
+  name        = "Europe"
+}
+
+resource "netbox_region" "england" {
+  name                 = "England"
+  parent_region_id     = netbox_region.europe.id
+}
+
+resource "netbox_region" "london" {
+  name             = "London"
+  parent_region_id = netbox_region.england.id
+}
