45bd547962eaed9f25f30a0908c4a5a1c6c2bc45
DontDDoS <git@dontddos.me>
2026-07-24 19:06:08 +0200

    Create prod-01 monitors for prometheus and grafana

---
+++ grafana-prod-01.tf
@@ -0,0 +1,11 @@
+resource "uptimekuma_monitor_http" "grafana-prod-01" {
+  name                  = "Grafana-Prod-01"
+  url                   = "https://grafana-prod-01.mon-prod-01.internal.dontddos.me"
+  interval              = 60
+  timeout               = 30
+  max_retries           = 2
+  retry_interval        = 60
+  active                = true
+  accepted_status_codes = ["200-299"]
+  parent                = uptimekuma_monitor_group.monitoring.id
+}
---
+++ prometheus-prod-01.tf
@@ -0,0 +1,11 @@
+resource "uptimekuma_monitor_http" "prometheus-prod-01" {
+  name                  = "Prometheus-Prod-01"
+  url                   = "https://prometheus-prod-01.mon-prod-01.internal.dontddos.me"
+  interval              = 60
+  timeout               = 30
+  max_retries           = 2
+  retry_interval        = 60
+  active                = true
+  accepted_status_codes = ["200-299"]
+  parent                = uptimekuma_monitor_group.monitoring.id
+}
