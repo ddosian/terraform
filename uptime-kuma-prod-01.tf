f57c354c4775dea0f61efc18dc090f636c4ba54e
DontDDoS <git@dontddos.me>
2026-07-24 17:54:27 +0200

    Created group for traefik

--- uptime-kuma-prod-01.tf
+++ uptime-kuma-prod-01.tf
@@ -44,6 +44,12 @@ resource "uptimekuma_monitor_group" "dns_monitor_group" {
   active = true
 }
 
+resource "uptimekuma_monitor_group" "traefik_monitor_group" {
+  name   = "Traefik"
+  parent = uptimekuma_monitor_group.networking_monitor_group.id
+  active = true
+}
+
 resource "uptimekuma_monitor_group" "storage_monitor_group" {
   name   = "Storage"
   parent = uptimekuma_monitor_group.infrastructure_monitor_group.id
