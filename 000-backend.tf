1ddaf14930f8a9201f7c5dfcba57a084d5d18187
DontDDoS <git@dontddos.me>
2026-07-25 10:57:37 +0200

    Migrated tfstate to B2 bucket via s3

---
+++ 000-backend.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket = "ddostfstate"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+
+    profile = "b2"
+
+    endpoints = {
+      s3 = "https://s3.us-west-004.backblazeb2.com"
+    }
+
+    skip_requesting_account_id   = true
+    skip_credentials_validation  = true
+    skip_metadata_api_check      = true
+    skip_region_validation       = true
+    use_path_style               = true
+  }
+}
