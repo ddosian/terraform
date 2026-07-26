18c3df1f844997a020c13fb91ea2788537acd0c8
DontDDoS <git@dontddos.me>
2026-07-26 14:32:20 +0200

    Removed OS nodes

--- os-controller-test-01.tf
+++
@@ -1,8 +0,0 @@
-module "os-controller-test-01_vm" {
-  source = "./modules/virtual-machine"
-
-  name        = "os-controller-test-01"
-  id          = 122
-  host        = "pve-prod-01"
-  ip_address  = "10.77.1.122/16" 
-}
--- os-controller-test-02.tf
+++
@@ -1,9 +0,0 @@
-module "os-controller-test-02_vm" {
-  source = "./modules/virtual-machine"
-
-  name        = "os-controller-test-02"
-  id          = 123
-  host        = "pve-prod-02"
-  disk_storage = "pve-prod-02-data-01"
-  ip_address  = "10.77.1.123/16" 
-}
