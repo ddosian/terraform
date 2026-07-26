04f87429eb7907071dc22c63624038e7f9acace3
DontDDoS <git@dontddos.me>
2026-07-26 12:20:31 +0200

    Created os-controller-test-02/03

---
+++ os-controller-test-02.tf
@@ -0,0 +1,8 @@
+module "os-controller-test-02_vm" {
+  source = "./modules/virtual-machine"
+
+  name        = "os-controller-test-02"
+  id          = 123
+  host        = "pve-prod-02"
+  ip_address  = "10.77.1.123/16" 
+}
---
+++ os-controller-test-03.tf
@@ -0,0 +1,9 @@
+module "os-controller-test-03_vm" {
+  source = "./modules/virtual-machine"
+
+  name        = "os-controller-test-03"
+  id          = 124
+  host        = "pve-prod-03"
+  network_bridge = "vmbr1"
+  ip_address  = "10.77.1.124/16" 
+}
