34498c37d5709ca332527b312dd02c8450fc60d0
DontDDoS <git@dontddos.me>
2026-07-26 12:31:22 +0200

    Fixed network bridge

--- os-controller-test-03.tf
+++ os-controller-test-03.tf
@@ -4,7 +4,7 @@ module "os-controller-test-03_vm" {
   name        = "os-controller-test-03"
   id          = 124
   host        = "pve-prod-03"
-  network_bridge = "vmbr1"
+  network_bridge = "vmbr0"
   disk_storage = "local-lvm"
   ip_address  = "10.77.1.124/16" 
 }
