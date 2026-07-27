3c79cfe05efb139a751134525e8b6326c8090257
DontDDoS <git@dontddos.me>
2026-07-27 15:27:19 +0200

    Deleted nextcloud-prod-01

--- nextcloud-prod-01.tf
+++
@@ -1,9 +0,0 @@
-resource "cloudflare_dns_record" "nextcloud-prod-01_cloudflare_record" {
-  zone_id = "310137280dfff667dd7414c94ef3a938"
-  name    = "cloud.dontddos.me"
-  ttl     = 1
-  type    = "A"
-  comment = "Managed by Terraform"
-  content = "156.67.29.169"
-  proxied = false
-}
