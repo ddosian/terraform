7d58dc5d064001425fa8200c9587b67a0d562b0a
DontDDoS <git@dontddos.me>
2026-07-27 10:37:32 +0200

    Split uptime-kuma-prod-01.tf into seperate files

---
+++ uptime-kuma-groups.tf
@@ -0,0 +1,92 @@
+resource "uptimekuma_monitor_group" "infrastructure_monitor_group" {
+  name   = "Infrastructure"
+  active = true
+}
+
+resource "uptimekuma_monitor_group" "kubernetes_monitor_group" {
+  name   = "Kubernetes"
+  parent = uptimekuma_monitor_group.infrastructure_monitor_group.id
+  active = true
+}
+
+resource "uptimekuma_monitor_group" "k3s-cl-prod-01_monitor_group" {
+  name   = "K3s-Cl-Prod-01"
+  parent = uptimekuma_monitor_group.kubernetes_monitor_group.id
+  active = true
+}
+
+resource "uptimekuma_monitor_group" "k3s-cl-prod-02_monitor_group" {
+  name   = "K3s-Cl-Prod-02"
+  parent = uptimekuma_monitor_group.kubernetes_monitor_group.id
+  active = true
+}
+
+resource "uptimekuma_monitor_group" "proxmox-ve_monitor_group" {
+  name   = "Proxmox VE"
+  parent = uptimekuma_monitor_group.infrastructure_monitor_group.id
+  active = true
+}
+
+resource "uptimekuma_monitor_group" "networking_monitor_group" {
+  name   = "Networking"
+  active = true
+}
+
+resource "uptimekuma_monitor_group" "routing_monitor_group" {
+  name   = "Routing"
+  parent = uptimekuma_monitor_group.networking_monitor_group.id
+  active = true
+}
+
+resource "uptimekuma_monitor_group" "dns_monitor_group" {
+  name   = "DNS"
+  parent = uptimekuma_monitor_group.networking_monitor_group.id
+  active = true
+}
+
+resource "uptimekuma_monitor_group" "traefik_monitor_group" {
+  name   = "Traefik"
+  parent = uptimekuma_monitor_group.networking_monitor_group.id
+  active = true
+}
+
+resource "uptimekuma_monitor_group" "storage_monitor_group" {
+  name   = "Storage"
+  parent = uptimekuma_monitor_group.infrastructure_monitor_group.id
+  active = true
+}
+
+resource "uptimekuma_monitor_group" "edge_monitor_group" {
+  name   = "Edge"
+  parent = uptimekuma_monitor_group.infrastructure_monitor_group.id
+  active = true
+}
+
+resource "uptimekuma_monitor_group" "vms_monitor_group" {
+  name   = "VMs"
+  parent = uptimekuma_monitor_group.infrastructure_monitor_group.id
+  active = true
+}
+
+resource "uptimekuma_monitor_group" "services" {
+  name   = "Services"
+  active = true
+}
+
+resource "uptimekuma_monitor_group" "media" {
+  name   = "Media"
+  parent = uptimekuma_monitor_group.services.id
+  active = true
+}
+
+resource "uptimekuma_monitor_group" "git" {
+  name   = "Git"
+  parent = uptimekuma_monitor_group.services.id
+  active = true
+}
+
+resource "uptimekuma_monitor_group" "monitoring" {
+  name   = "Monitoring"
+  parent = uptimekuma_monitor_group.services.id
+  active = true
+}
