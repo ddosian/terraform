0db5d3dc7d9581c17a73b33c0551e2fede5ba1bb
DontDDoS <git@dontddos.me>
2026-08-17 13:55:37 +0200

    Created nc-prod-01

---
+++ nc-prod-01.tf
@@ -0,0 +1,12 @@
+module "nc-prod-01_vm" {
+  source = "./modules/virtual-machine"
+
+  name       = "nc-prod-01"
+  id         = 122
+  host       = "pve-prod-01"
+  vlan_tag   = 78
+  ip_address = "10.78.1.122/16"
+  gateway    = "10.78.0.3"
+
+  monitor_group = uptimekuma_monitor_group.vms_monitor_group.id
+}
