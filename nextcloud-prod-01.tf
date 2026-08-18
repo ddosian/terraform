aad35f5976aab4d6ea4216067c53013702a0c5d5
DontDDoS <git@dontddos.me>
2026-08-18 17:38:10 +0100

    Created uptime kuma monitor for nextcloud-prod-01

--- nextcloud-prod-01.tf
+++ nextcloud-prod-01.tf
@@ -8,3 +8,19 @@ resource "cloudflare_dns_record" "nextcloud-prod-01_cloudflare_record" {
   content = "156.67.29.169"
   proxied = false
 }
+
+resource "uptimekuma_monitor_http" "nextcloud-prod-01" {
+  name                  = "Nextcloud-Prod-01"
+  url                   = "https://nextcloud.dontddos.me"
+  interval              = 60
+  timeout               = 30
+  max_retries           = 2
+  retry_interval        = 60
+  active                = true
+  accepted_status_codes = ["200-299"]
+  notification_ids = [
+    1,
+    2,
+  ]
+  parent = uptimekuma_monitor_group.storage.id
+}
