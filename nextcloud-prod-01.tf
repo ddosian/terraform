e1fb5e375bac0e4b18a3fc5aca5b1b35613ec4c1
DontDDoS <git@dontddos.me>
2026-08-18 17:04:47 +0100

    Created DNS record for nextcloud-prod-01

---
+++ nextcloud-prod-01.tf
@@ -0,0 +1,10 @@
+# Cloudflare DNS records
+resource "cloudflare_dns_record" "nextcloud-prod-01_cloudflare_record" {
+  zone_id = "310137280dfff667dd7414c94ef3a938"
+  name    = "nextcloud-prod-01.dontddos.me"
+  ttl     = 1
+  type    = "A"
+  comment = "Managed by Terraform"
+  content = "156.67.29.169"
+  proxied = false
+}
